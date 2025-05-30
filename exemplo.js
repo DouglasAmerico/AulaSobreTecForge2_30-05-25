f4606b9fbdec64bccd00b64dfd8fa305e43523af
Douglas Américo <douglas@likesistemas.com.br>
2025-05-30 20:05:00 -0300

    revisao tf1

--- exemplo.js
+++ exemplo.js
@@ -1,1 +1,25 @@
-console.log("Hello Word");
+const prompt = require("prompt-sync")();
+let qtd;
+do{
+    qtd = prompt("Quantos pessoas quer intrevistar? ");
+    if(qtd <= 0) {
+        console.log("O numero de pessoas tem que ser maior que 0");
+    }
+} while (qtd <= 0);
+
+let nome, idade,qtdPessoasMaioresDeIdade=0;
+
+for(let i=0;i<qtd;i++){
+    nome = prompt("Informe o nome ");
+    do{
+        idade = prompt("Informe a idade do(a) "+nome+": ");
+        if(idade < 0 || idade > 120){
+            console.log("Idades validas são entre 0 e 120 anos");
+        }
+    } while(idade < 0 || idade > 120);    
+    if(idade >= 18){
+        qtdPessoasMaioresDeIdade++;
+    }
+}
+
+console.log("Fora informadas "+qtd+" pessoas e "+qtdPessoasMaioresDeIdade+" são maiores de idade");
